f10cf38b14629a95578b24ea2e238addad3606db
Omar <om749499@tutanota.de>
2026-04-16 20:43:35 +0200

    chore: bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.omardotdev.lumi"
         minSdk = 24
         targetSdk = 36
-        versionCode = 11
-        versionName = "1.2.1"
+        versionCode = 12
+        versionName = "1.2.2"
     }
 
     dependenciesInfo {
